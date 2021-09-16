c615180a596e94b9e5eea3bdf507752388bb53c7
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-09-16 18:02:40 +0300

    add app component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { fetchData, fetchDailyData, fetchCountries } from './api';
+import { Cards, CountryPicker, Chart } from './components';
+import styles from './App.module.css';
+class App extends Component {
+  state = {
+    data: {},
+    country: '',
+  };
 
-function App() {
-  return <div className="App"></div>;
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country });
+  };
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
 }
 
 export default App;
